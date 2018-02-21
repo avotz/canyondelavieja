ef1ac60cb677ebc9211357529ff4986cc3f5b54a
Alonso Avotz <alonso@avotz.com>
2018-02-21 10:29:50 -0600

    se actualizo home con packages

--- wp-content/themes/canyon/template-parts/content-featured-services.php
+++ wp-content/themes/canyon/template-parts/content-featured-services.php
@@ -155,70 +155,89 @@
 			<h2 class="packages-title services-title">Featured Packages</h2>
 			<a href="<?php echo esc_url(home_url('/packages')); ?>">View more</a>
 			</div>
+
+			<?php
+		$args = array(
+			'post_type' => 'product',
+			'orderby' => array('menu_order' => 'ASC', 'title' => 'ASC'),
+			'posts_per_page' => 3,
+			'tax_query' => array(
+				array(
+					'taxonomy' => 'product_cat',
+					'field' => 'slug',
+					'terms' => 'packages'
+				)
+			)
+
+
+		);
+
+
+		$items = new WP_Query($args);
+					// Pagination fix
+		$temp_query = $wp_query;
+		$wp_query = null;
+		$wp_query = $items;
+		$index = 0;
+		if ($items->have_posts()) {
+			while ($items->have_posts()) {
+				$items->the_post();
+
+				?>
+				<?php if (has_post_thumbnail()) :
+
+				$id = get_post_thumbnail_id($post->ID);
+				$thumb_url = wp_get_attachment_image_src($id, 'item-home-thumb', true);
+
+				?>
+					<?php else :
+					$thumb_url = ''; ?>     
+									
+						<?php endif; ?>
+						<article class="packages-item services-item">
+						<div class="packages-item-img services-item-img">
+							<img src="<?php echo $thumb_url[0]; ?>">
+						</div>
+						<div class="packages-item-content services-item-content">
+							<h3 class="services-item-title"><?php the_title(); ?></h3>
+							<!-- <div class="services-item-subtitle">loremp ipsum, loremp ipsum</div> -->
+							<?php the_excerpt(); ?>
+						</div>
+						<div class="packages-item-details services-item-details">
+						<div class="top">
+							<div class="price">
+								<?php $currency = get_woocommerce_currency_symbol();
+
+								$product = new WC_Product($post->ID); 
+										/*echo $product->get_price_html();
+										
+										woocommerce_template_loop_price(); */
+								echo $currency;
+
+								if (get_post_meta(get_the_ID(), '_wc_display_cost', true))
+									echo get_post_meta(get_the_ID(), '_wc_display_cost', true);
+								else
+									echo get_post_meta(get_the_ID(), '_wc_booking_cost', true) ?>
+							</div>
+						</div>
+						<div class="bottom">
+							<a href="<?php the_permalink(); ?>" class="btn success">Read more</a>
+						</div>
+						</div>
+					</article>
+						
+				
+					
+					<?php
+
+				$index++;
+			}
+		}
+
+		?>
 			
-			<article class="packages-item services-item">
-				<div class="packages-item-img services-item-img">
-				<img src="<?php echo get_template_directory_uri(); ?>/img/package-1.jpg">
-				</div>
-				<div class="packages-item-content services-item-content">
-					<h3 class="services-item-title">loremp ipsum</h3>
-					<div class="services-item-subtitle">loremp ipsum, loremp ipsum</div>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat.</p>
-				</div>
-				<div class="packages-item-details services-item-details">
-				<div class="top">
-					<div class="price">$129</div>
-				</div>
-				<div class="bottom">
-					<a href="#" class="btn success">Read more</a>
-				</div>
-				</div>
-			</article>
-			<article class="packages-item services-item">
-				<div class="packages-item-img services-item-img">
-				<img src="<?php echo get_template_directory_uri(); ?>/img/package-2.jpg">
-				</div>
-				<div class="packages-item-content services-item-content">
-					<h3 class="services-item-title">loremp ipsum</h3>
-					<div class="services-item-subtitle">loremp ipsum, loremp ipsum</div>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. </p>
-				</div>
-				<div class="packages-item-details services-item-details">
-				<div class="top">
-					<div class="price">$129</div>
-				</div>
-				<div class="bottom">
-					<a href="#" class="btn success">Read more</a>
-				</div>
-				</div>
-			</article>
-			<article class="packages-item services-item">
-				<div class="packages-item-img services-item-img">
-				<img src="<?php echo get_template_directory_uri(); ?>/img/package-3.jpg">
-				</div>
-				<div class="packages-item-content services-item-content">
-					<h3 class="services-item-title">loremp ipsum</h3>
-					<div class="services-item-subtitle">loremp ipsum, loremp ipsum</div>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat.</p>
-				</div>
-				<div class="packages-item-details services-item-details">
-				<div class="top">
-					<div class="price">$129</div>
-				</div>
-				<div class="bottom">
-					<a href="#" class="btn success">Read more</a>
-				</div>
-				</div>
-			</article>
+			
+			
 		</div>
 	</div>
 </section>
