60cd944e9b647a94eb4a6e04fbf3ef64557ee7b8
Alonso Avotz <alonso@avotz.com>
2018-02-22 16:46:33 -0600

    traduciendo order details

--- wp-content/themes/canyon/inc/translate_strings.php
+++ wp-content/themes/canyon/inc/translate_strings.php
@@ -33,6 +33,8 @@ if (function_exists('pll_register_string')) {
     pll_register_string('has been added to your cart', 'has been added to your cart', 'ALO');
     pll_register_string('Checkout', 'Checkout', 'ALO');
     pll_register_string('View Cart', 'View Cart', 'ALO');
+    pll_register_string('Order details', 'Order details', 'ALO');
+   
     pll_register_string('Proceed to checkout', 'Proceed to checkout', 'ALO');
     
     
